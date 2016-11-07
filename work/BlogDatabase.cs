1cd620e2212d7a0d328498b03ee0ef25ee92337e
stu <stu@qq.com>
2016-11-07 17:23:56 +0800

    验证标题、文章信息

--- work/BlogDatabase.cs
+++ work/BlogDatabase.cs
@@ -1,6 +1,7 @@
 ﻿namespace work
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
     using System.Data.Entity;
     using System.Linq;
 
@@ -62,12 +63,14 @@
         /// <summary>
         /// 标题
         /// </summary>
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 5)]
         public string Subject { get; set; }
 
         /// <summary>
         /// 文章内容
         /// </summary>
-
+        [Required]
         public string Body { get; set; }
 
         /// <summary>
